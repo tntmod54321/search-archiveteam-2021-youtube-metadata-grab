f6c6fedd81225b38a6175d8c62347559d899d1dc
Tntmod54321 <45548254+tntmod54321@users.noreply.github.com>
2022-09-07 05:01:36 -0400

    demo, dumps titles from small zst files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # search archiveteam 2021 youtube metadata grab
  Search jopik/archiveteam's december 2021 youtube metadata grab
+
+tested on windows 10 with python 3.9
+
+update --help  
+
---
+++ search.py
@@ -0,0 +1,50 @@
+if __name__ != "__main__":
+	print("not running as main, exiting...")
+	exit()
+
+import re
+import sys
+import json
+import zstandard as zstd
+import io
+
+files_folder = ""
+query_json = ""
+outputdir = ""
+file_offset = 0
+
+def printHelp():
+	# print sections for different downloaders (programmatically -> _downloaders -> downloader.info["help"]
+	# list valid ebay resolutions (leave blank for the max available)
+	print("Usage:", "py ebay_archive.py [ID or URL] [OUTPUT DIRECTORY] [ADDITIONAL ARGUMENTS]\n\n"+
+	"Arguments:\n", "  -h, --help\t-\tdisplay this usage info")
+	exit()
+
+if len(sys.argv[1:]) == 0: printHelp()
+i=1 # for arguments like [--command value] get the value after the command
+# first arg in sys.argv is the python file
+for arg in sys.argv[1:]:
+	if (arg in ["help", "/?", "-h", "--help"]): printHelp()
+	if (arg == "--offset"): file_offset = int(sys.argv[1:][i])
+	if (arg in ["-o", "--output-dir"]): outputdir = sys.argv[1:][i]
+	if (arg in ["-q", "--query-json"]): query_json = sys.argv[1:][i]
+	if (arg in ["-i", "--input-folder", "--files-folder"]): files_folder = sys.argv[1:][i]
+	
+	i+=1
+
+# if "" in [files_folder, query_json, outputdir]: printHelp()
+
+# Reusing a ZstdCompressor or ZstdDecompressor instance for multiple operations is
+# faster than instantiating a new ZstdCompressor or ZstdDecompressor for each operation
+
+file = sys.argv[1]
+with io.BytesIO() as dcBuffer:
+	with open(file, "rb") as f:
+		dctx = zstd.ZstdDecompressor()
+		dcBuffer.write(dctx.decompress(f.read(), max_output_size=7340032))# 7GB max filesize
+	dcBuffer.seek(0)
+	
+	for line in dcBuffer.readlines():
+		print(json.loads(line)["title"])
+
+exit()
