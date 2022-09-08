76af710176652b4ebc8b7fc367df16c35bceaddb
Tntmod54321 <45548254+tntmod54321@users.noreply.github.com>
2022-09-08 04:45:10 -0400

    move code to main()

--- search.py
+++ search.py
@@ -19,41 +19,7 @@ import os
 global reading_file
 global shutdownflag
 global L
-reading_file=False
-shutdownflag=False
-
-files_folder = ""
-query_json = ""
-outputdir = ""
-management_file = ""
-workerthreads = 1 # change default to 2 or 4
-
-### parse args
-# update help message
-def printHelp():
-	# print sections for different downloaders (programmatically -> _downloaders -> downloader.info["help"]
-	# list valid ebay resolutions (leave blank for the max available)
-	print("Usage:", "py ebay_archive.py [ID or URL] [OUTPUT DIRECTORY] [ADDITIONAL ARGUMENTS]\n\n"+
-	"Arguments:\n", "  -h, --help\t-\tdisplay this usage info")
-	exit()
-
-if len(sys.argv[1:]) == 0: printHelp()
-i=1 # for arguments like [--command value] get the value after the command
-# first arg in sys.argv is the python file
-for arg in sys.argv[1:]:
-	if (arg in ["help", "/?", "-h", "--help"]): printHelp()
-	if (arg in ["-o", "--output-dir"]): outputdir = sys.argv[1:][i]
-	if (arg in ["-q", "--query-json"]): query_json = sys.argv[1:][i]
-	if (arg in ["-i", "--input-folder", "--files-folder"]): files_folder = sys.argv[1:][i]
-	if (arg in ["-m", "--search-management-file"]): management_file = sys.argv[1:][i]
-	if (arg in ["-t", "--threads"]): workerthreads = int(sys.argv[1:][i])
-	
-	i+=1
-
-if "" in [files_folder, query_json, outputdir, management_file]: printHelp() # these args are critical, print help if not present
-if workerthreads<1:
-	print("1 thread minimum")
-	exit()
+global results
 
 ### build list of zstd files
 def find_files(fdir, fext):
@@ -75,26 +41,6 @@ def find_files(fdir, fext):
 	print("found {} {} files in {}".format(len(files2), fext, fdir))
 	return files2
 
-zstdfiles = find_files(files_folder, ".zst")
-zstdfiles.sort()
-
-### load queries
-with open(query_json, "rb") as f:
-	queries={}
-	x = json.loads(f.read().decode("utf-8"))
-	for y in x:
-		for expression in y["expressions"]:
-			queries[expression] = y["filename"]
-	results_files = []
-	for y in x:
-		results_files.append(y["filename"])
-
-### create output folder
-try:
-	makedirs(outputdir)
-except FileExistsError:
-	pass
-
 def writemsg(msg):
 	sys.stdout.write(msg)
 	sys.stdout.flush()
@@ -104,10 +50,11 @@ def searchline(string, expression):
 	if re.search(expression, string): result=True
 	return result
 
-def worker(tid, fbuffer, expressions, results):
+def worker(tid, fbuffer, expressions):
 	global reading_file
 	global shutdownflag
 	global L
+	global results
 	
 	while True:
 		try:
@@ -153,73 +100,135 @@ def writeResults(results):
 	
 	return
 
-### load management file if exists
-if isfile(management_file):
-	with open(management_file, "rb") as f:
-		completed_files = json.loads(f.read().decode("utf-8"))
-else:
-	completed_files = []
+reading_file=False
+shutdownflag=False
 
-### search zstd files
-executor = ThreadPoolExecutor(max_workers=16) # will limit the amount of threads
-dctx = zstd.ZstdDecompressor() # reuse decompressor object (docs recommend, woohoo!)
-try:
-	for file in zstdfiles: # should remove searched files listed in management file earlier
-		if file in completed_files:
-			print(f"skipping file {file} (completed)")
-			continue
+files_folder = ""
+query_json = ""
+outputdir = ""
+management_file = ""
+workerthreads = 1 # change default to 2 or 4
+
+### parse args
+# update help message
+def printHelp():
+	# print sections for different downloaders (programmatically -> _downloaders -> downloader.info["help"]
+	# list valid ebay resolutions (leave blank for the max available)
+	print("Usage:", "py ebay_archive.py [ID or URL] [OUTPUT DIRECTORY] [ADDITIONAL ARGUMENTS]\n\n"+
+	"Arguments:\n", "  -h, --help\t-\tdisplay this usage info")
+	exit()
+
+def main():
+	global reading_file
+	global shutdownflag
+	global L
+	global results
+	
+	if len(sys.argv[1:]) == 0: printHelp()
+	i=1 # for arguments like [--command value] get the value after the command
+	# first arg in sys.argv is the python file
+	for arg in sys.argv[1:]:
+		if (arg in ["help", "/?", "-h", "--help"]): printHelp()
+		if (arg in ["-o", "--output-dir"]): outputdir = sys.argv[1:][i]
+		if (arg in ["-q", "--query-json"]): query_json = sys.argv[1:][i]
+		if (arg in ["-i", "--input-folder", "--files-folder"]): files_folder = sys.argv[1:][i]
+		if (arg in ["-m", "--search-management-file"]): management_file = sys.argv[1:][i]
+		if (arg in ["-t", "--threads"]): workerthreads = int(sys.argv[1:][i])
 		
-		writemsg(f"searching {file}:\n")
-		with open(file, "rb") as f:
-			dobj = dctx.stream_reader(f.read())
-			dbuf = io.BufferedReader(dobj)
-			now=time.time()
-			L=0
-			futures=[]
-			results={}
-			reading_file = False
-			threads_dead=False
-			for i in range(0, workerthreads):
-				future = executor.submit(worker, i, dbuf, queries, results)
-				futures.append(future)
-			
-			# wait for workers to die
-			while True:
-				if shutdownflag:
-					print("thread requested shutdown")
-					os._exit(0)
-				
-				if results: writeResults(results) # make sure to dump all results to disk before moving on to next file
-				
-				future_statuses=[]
-				for future in futures:
-					future_statuses.append(future.done())
-				
-				if threads_dead: break # leave while loop after flushing results
-				
-				# check if all threads are dead
-				if all(future_statuses): # we do another loop to make sure we flush the results to disk
-					threads_dead=True
-					continue
-				
-				# time.sleep(0.01)
-				writemsg('.')
-				time.sleep(0.1)
-				# time.sleep(5)
-		
-		### update management file
-		completed_files.append(file)
-		with open(management_file, "wb") as f:
-			f.write(json.dumps(completed_files).encode("utf-8"))
-		
+		i+=1
+
+	if "" in [files_folder, query_json, outputdir, management_file]: printHelp() # these args are critical, print help if not present
+	if workerthreads<1:
+		print("1 thread minimum")
 		exit()
-		sys.stdout.write('\n')
-		elapsed=time.time()-now
-		print(f"took {elapsed} to search {L} lines ({round(L/elapsed, 2)}/s)")
-except KeyboardInterrupt:
-	print("\nInterrupted!")
 
-os._exit(0)
+	zstdfiles = find_files(files_folder, ".zst")
+	zstdfiles.sort()
+
+	### load queries
+	with open(query_json, "rb") as f:
+		queries={}
+		x = json.loads(f.read().decode("utf-8"))
+		for y in x:
+			for expression in y["expressions"]:
+				queries[expression] = y["filename"]
+		results_files = []
+		for y in x:
+			results_files.append(y["filename"])
+
+	### create output folder
+	try:
+		makedirs(outputdir)
+	except FileExistsError:
+		pass
+
+	### load management file if exists
+	if isfile(management_file):
+		with open(management_file, "rb") as f:
+			completed_files = json.loads(f.read().decode("utf-8"))
+	else:
+		completed_files = []
+
+	### search zstd files
+	executor = ThreadPoolExecutor(max_workers=16) # will limit the amount of threads
+	dctx = zstd.ZstdDecompressor() # reuse decompressor object (docs recommend, woohoo!)
+	try:
+		for file in zstdfiles: # should remove searched files listed in management file earlier
+			if file in completed_files:
+				print(f"skipping file {file} (completed)")
+				continue
+			
+			writemsg(f"searching {file}:\n")
+			with open(file, "rb") as f:
+				dobj = dctx.stream_reader(f.read())
+				dbuf = io.BufferedReader(dobj)
+				now=time.time()
+				L=0
+				futures=[]
+				results={}
+				reading_file = False
+				threads_dead=False
+				for i in range(0, workerthreads):
+					future = executor.submit(worker, i, dbuf, queries)
+					futures.append(future)
+				
+				# wait for workers to die
+				while True:
+					if shutdownflag:
+						print("thread requested shutdown")
+						os._exit(0)
+					
+					if results: writeResults(results) # make sure to dump all results to disk before moving on to next file
+					
+					future_statuses=[]
+					for future in futures:
+						future_statuses.append(future.done())
+					
+					if threads_dead: break # leave while loop after flushing results
+					
+					# check if all threads are dead
+					if all(future_statuses): # we do another loop to make sure we flush the results to disk
+						threads_dead=True
+						continue
+					
+					# time.sleep(0.01)
+					writemsg('.')
+					time.sleep(0.1)
+					# time.sleep(5)
+			
+			### update management file
+			completed_files.append(file)
+			with open(management_file, "wb") as f:
+				f.write(json.dumps(completed_files).encode("utf-8"))
+			
+			sys.stdout.write('\n')
+			elapsed=time.time()-now
+			print(f"took {elapsed} to search {L} lines ({round(L/elapsed, 2)}/s)")
+	except KeyboardInterrupt:
+		print("\nInterrupted!")
+		os._exit(0)
+
+main()
 
 # to add:
 # better logging (time taken, offset, results count, etc)
