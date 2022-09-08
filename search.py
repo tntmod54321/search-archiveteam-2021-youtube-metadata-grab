33fe01ffab6e766b0267a885bd9cafe0bfc850dc
Tntmod54321 <45548254+tntmod54321@users.noreply.github.com>
2022-09-08 00:04:32 -0400

    Update search.py

--- search.py
+++ search.py
@@ -10,6 +10,10 @@ import io
 import time
 from os import listdir, makedirs
 from os.path import isfile, isdir, splitext, split
+from multiprocessing.pool import ThreadPool
+import concurrent.futures
+from concurrent.futures import *
+import traceback
 
 files_folder = ""
 query_json = ""
@@ -19,7 +23,7 @@ file_offset = 0
 
 #I'm dumb idk if this works properly for numbers that aren't multiples of 10
 stdout_update_interval=1000 # print a '.' every x line searches
-threads = 2
+workerthreads = 2
 
 # need to compare regexing the line 80 times vs 
 # loading as json and regexing some fields
@@ -42,13 +46,13 @@ for arg in sys.argv[1:]:
 	if (arg in ["-q", "--query-json"]): query_json = sys.argv[1:][i]
 	if (arg in ["-i", "--input-folder", "--files-folder"]): files_folder = sys.argv[1:][i]
 	if (arg in ["-m", "--search-management-file"]): management_file = sys.argv[1:][i]
-	if (arg in ["-t", "--threads"]): threads = int(sys.argv[1:][i])
+	if (arg in ["-t", "--threads"]): workerthreads = int(sys.argv[1:][i])
 	
 	i+=1
 
 if "" in [files_folder, query_json, outputdir]: printHelp() # these args are critical, print help if not present
-if threads<2:
-	print("2 threads minimum (manager+1 worker)")
+if workerthreads<1:
+	print("1 thread minimum")
 	exit()
 
 ### build list of zstd files
@@ -104,6 +108,19 @@ def searchline(string, expression):
 	if re.search(expression, string): result=True
 	return result
 
+def worker(string, expressions):
+	results=[]
+	try:
+		
+	except:
+		open("errors", "w+") as f:
+			f.write()
+	return results
+
+# <-- here
+# load management file,
+# remove duplicates from file list to be iterated upon by the decompress/search thingy
+
 ### search zstd files
 i=0
 i+=file_offset # keep track of offset
@@ -120,13 +137,31 @@ try:
 				results[querycat["filename"]] = []
 			
 			l=0
-			writemsg(f"searching {file}:\n") # remove newline later
+			writemsg(f"searching {file}:\n")
 			while True:
 				line = dbuf.readline()
 				if not line: break
 				
-				# search line here
 				str_line=line.decode("utf-8")
+				
+				# maybe make the expressions list 
+				
+				# create jobs
+				futures=[]
+				###create worker threads
+				for worker in range(0, requeststhreads):
+					future = executor.submit(worker, str_line, expressions)
+					futures.append(future)
+				
+				while True:
+					for future in futures:
+						
+					time.sleep(0.01)
+				
+				result=[]
+				# wait for jobs
+				# result+= futures
+				
 				for querycat in queries:
 					queryfilename=querycat["filename"] # remove this
 					for ex in querycat["expressions"]:
@@ -134,18 +169,16 @@ try:
 						# if we match with this line for this expression then
 						# append this line to the results for the filename
 						# it's assigned to
+							
 						if searchline(str_line, ex): results[queryfilename].append(line)
 				
 				l+=1
 				if (l/stdout_update_interval).is_integer():
 					writemsg('.')
-			# balls
-			elapsed=time.time()-now
 			print()
-			print(results)
-			print(elapsed)
+			print(time.time()-now)
 		
-		# l is equal to the number of lines checked
+		print(f"checked {l} lines")
 		
 		sys.stdout.write('\n')
 		i+=1
@@ -170,3 +203,8 @@ exit()
 
 # multithread the regex searching
 # round-robin assign the threads expressions and pass them each the line
+# or assign a line to each thread
+# is spawning a bunch of threads and waiting for them each line slow
+
+# if you do the thread-per-line thing then maybe make the main thread spawn more threads
+# when the workers are done, time.sleep(0.01)?
